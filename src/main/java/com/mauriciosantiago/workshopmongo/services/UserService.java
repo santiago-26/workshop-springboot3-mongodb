14d90cb5f12aa2c10d678edf91d39919e070e5f7
Mauricio Santiago <mauriciosantiago95@hotmail.com>
2025-02-09 11:28:53 -0300

    insercao de usuario com post

--- src/main/java/com/mauriciosantiago/workshopmongo/services/UserService.java
+++ src/main/java/com/mauriciosantiago/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mauriciosantiago.workshopmongo.DTO.UserDTO;
 import com.mauriciosantiago.workshopmongo.domain.User;
 import com.mauriciosantiago.workshopmongo.repository.UserRepository;
 import com.mauriciosantiago.workshopmongo.services.exception.ObjectNotFoundException;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert (User obj) {
+		return repo.save(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
 	}
 }
